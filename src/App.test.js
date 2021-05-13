af3e5bee602aed32c6134def5fc9bb3694efc88e
Thaisa <thaisa.tps@gmail.com>
2021-05-13 20:55:01 -0300

    firt commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,14 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  return expect(linkElement).toBeInTheDocument();
 });
+
+
+
+test('renders learn react link 2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  return expect(linkElement).toBeInTheDocument();
+});
+
